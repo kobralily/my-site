8a8380e63d03f50164ad596bac39ad5cdce3ee4d
kobralily <87397507+kobralily@users.noreply.github.com>
2023-12-09 03:48:36 -0500

    good name

--- ufsemesterplanner/scraping/flaskapp.py
+++ ufsemesterplanner/scraping/flaskapp.py
@@ -1,39 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-from bs4 import BeautifulSoup
-import requests
+from flask import Flask, render_template, request, send_from_directory
+from scrapeclasses import scrapeClasses
 import json
-from scrapeclasses import *
 import os
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-# Set the static folder to 'static'
-#app.static_folder = 'static'
+    @app.route('/', methods=['GET'])
+    def index():
+        return render_template('index.html')
 
-# def scrapePlan(major, major2, minor, certificate):
+    @app.route('/modelsemester', methods=['POST'])
+    def modelsemester():
+        major_name = request.form.get('majors')
+        classesArray = scrapeClasses(major_name)
+        classesArrayJson = json.dumps(classesArray)
+        return render_template('modelsemester.html', classesArrayJson=classesArrayJson)
 
-#     return scrapeClasses(major), scrapeClasses(major2), scrapeClasses(minor), scrapeClasses(certificate)
+    @app.route('/favicon.ico')
+    def favicon():
+        return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/', methods=['GET'])
-def index():
-    #classesArray = scrapeClasses("Accounting")
-    #classesArrayJson = json.dumps(classesArray)
-    #print(classesArrayJson) #works
-    #return render_template('index.html', classesArrayJson = json.dumps(classesArray))
-    return render_template('index.html')
+    return app
 
-@app.route('/modelsemester', methods=['POST'])
-def modelsemester():
-    major_name = request.form.get('majors')
-    classesArray = scrapeClasses(major_name)
-    classesArrayJson = json.dumps(classesArray)
-    #return render_template('modelsemester.html')
-    return render_template('modelsemester.html', classesArrayJson = classesArrayJson)
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
+# This is only used when running locally
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=False, port=5500)
